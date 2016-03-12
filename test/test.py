7f20ebed7a51738a1260464c42d35b54c236ca7f
kne <sirkne@gmail.com>
2016-03-12 16:39:00 -0500

    pep8 and initialization cleanup

--- test/test.py
+++ test/test.py
@@ -8,35 +8,35 @@ sys.path.append(join(dirname(__file__), '..', 'src'))
 os.environ['LEAP_DLL_PATH'] = abspath(join(dirname(__file__), '..', 'dll'))
 
 # Import leap
-from cleap.leap import *
+from cleap import (leap, Event)
 
-c = leap_controller()
-l = leap_listener(500)
-leap_add_listener(c, l)
-leap_enable_background(c)
+c = leap.leap_controller()
+l = leap.leap_listener(500)
+leap.leap_add_listener(c, l)
+leap.leap_enable_background(c)
 
 try:
     while True:
-      waiting = False
-      while not waiting:
-        event = leap_poll_listener(l)
-        if event:
-          e = Event(event)
-          if not e.frame.hands:
-              continue
+        waiting = False
+        while not waiting:
+            event = leap.leap_poll_listener(l)
+            if event:
+                e = Event(event)
+                if not e.frame.hands:
+                    continue
 
-          print('----')
-          for hand in e.frame.hands:
-              print(hand)
-        else:
-          waiting = True
-      time.sleep(0.01)
+                print('----')
+                for hand in e.frame.hands:
+                    print(hand)
+            else:
+                waiting = True
+        time.sleep(0.01)
 except KeyboardInterrupt:
     pass
 finally:
     print()
     print('Done')
 
-    leap_remove_listener(c, l)
-    leap_listener_dispose(l)
-    leap_controller_dispose(c)
+    leap.leap_remove_listener(c, l)
+    leap.leap_listener_dispose(l)
+    leap.leap_controller_dispose(c)
