1a058a0b9abe4fc2935347cbc88eb7230afdcf12
kne <sirkne@gmail.com>
2016-03-12 17:10:21 -0500

    example initialization cleanup

--- src/cleap/__init__.py
+++ src/cleap/__init__.py
@@ -1,5 +1,2 @@
 from __future__ import absolute_import
 from .leap import *
-
-
-leap = initialize_cleap()
--- test/test.py
+++ test/test.py
@@ -1,15 +1,13 @@
 import os
-import sys
 import time
-from os.path import dirname, join, abspath
-sys.path.append(join(dirname(__file__), '..', 'src'))
 
-# Set DLL path for leap libraries
-os.environ['LEAP_DLL_PATH'] = abspath(join(dirname(__file__), '..', 'dll'))
+import cleap
 
-# Import leap
-from cleap import (leap, Event)
+script_path = os.path.dirname(__file__)
+# by default, this is looking for the leap library in ../dll
+library_path = os.path.abspath(os.path.join(script_path, '..', 'dll'))
 
+leap = cleap.initialize_cleap(library_path=library_path)
 c = leap.leap_controller()
 l = leap.leap_listener(500)
 leap.leap_add_listener(c, l)
@@ -21,7 +19,7 @@ try:
         while not waiting:
             event = leap.leap_poll_listener(l)
             if event:
-                e = Event(event)
+                e = cleap.Event(event)
                 if not e.frame.hands:
                     continue
 
